14a295c1249603926f50fa8cf3295776a2f2055d
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2022-04-04 23:08:43 +0300

    Добавили открытие попапа, делегирование, удаление обработчиков событий

--- js/script.js
+++ js/script.js
@@ -1,29 +1,50 @@
-var link = document.querySelector(".j-button-buy");
 var popup = document.querySelector(".j-modal");
 var btnClose = popup.querySelector(".j-modal-close");
+var cardsList = document.querySelector(".j-list-card");
 
+// Функция вызывается для закрытия попапа при нажатии на крестик
+var closePopupOnBtn = function() {
+    closePopup();
+}
 
-link.addEventListener("click", function(){
-    popup.style.display = "block";
-});
+// Функция вызывается для закрытия попапа при нажатии на кнопку ESC
+var closePopupOnEscPress = function(e) {
+    if (e.keyCode === 27) {
+        closePopup();
+    }
+}
 
-// Для пробы:
-// link.addEventListener("click", function(){
-//     // evt.preventDefault();
-//     // console.dir(popup);
-//     popup.classList.add("modal-show");
-// });
-
-btnClose.addEventListener("click", function(){
+// Общая функция закрытия попапа.
+// Скрывает попап стилями и удаляет ненужные обработчики событий.
+var closePopup = function() {
     popup.style.display = "none";
+    btnClose.removeEventListener("click", closePopupOnBtn);
+    window.removeEventListener("keydown", closePopupOnEscPress);
+}
+
+// Делегирование.
+// Вместо того, чтобы добавлять обработчик события на каждый элемент,
+// добавляем только один обработчик на общий контейнер.
+cardsList.addEventListener("click", function(e) {
+    // Если при клике на контейнер, мы попадаем в кнопку - то показываем попап, 
+    // и добавляем два обработчика событий для закрытия попапа.
+    if (e.target.classList.contains('j-button-buy')) {
+        popup.style.display = "block";
+        
+        // один для событий с клавиатуры
+        window.addEventListener("keydown", closePopupOnEscPress);
+        // второй при клике на крестик
+        btnClose.addEventListener("click", closePopupOnBtn);
+    }
 });
 
-// Это не заработало, на все кнопки
-// var link = document.querySelectorAll(".j-button-buy");
-// var popup = document.querySelector(".j-modal");
-// var btnClose = popup.querySelector(".j-modal-close");
+// var links = document.querySelectorAll(".j-button-buy");
 
-
-// link.addEventListenerAll("click", function(){
-//     popup.style.display = "block";
+// Foreach
+// links.forEach(function(elem) {
+//     elem.addEventListener("click", function(){
+//         popup.style.display = "block";
+        
+//         btnClose.addEventListener("click", closePopup);
+//     });
 // });
